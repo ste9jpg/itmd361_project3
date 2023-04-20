6064e07dd48cde1c9d5feaa22ede75e28fe36784
ste9jpg <stejpg9@gmail.com>
2023-04-20 11:35:51 -0500

    Update scriptt.js

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,4 +1,4 @@
-function initMap() {
+function init() {
   //alert('it works');
   var el = document.getElementById('canvas');
   var myLocation = new google.maps.LatLng(41.883796338138474, -87.65370045842398);
@@ -20,16 +20,23 @@ function initMap() {
     icon: 'media/m.png'
   });
 
-  var contentString = '<h2>Hamburger University</h2><p>Hamburger University is a training facility at the McDonald\'s Corporation global headquarters in Chicago, Illinois. It instructs high-potential restaurant managers, mid-managers and owner-operators in restaurant management.</p>';
+  var contentString = '<h2>Hamburger University</h2><p>Hamburger University is a training facility at the McDonald\'s Corporation global headquarters in Chicago, Illinois. It instructs high-potential restaurant managers, mid-managers and owner-operators in restaurant management.</p>'
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString
   });
 
+  google.maps.event.addListener(marker, 'mouseover', function() {
+	  infowindow.open(myMap, marker);
+  });
+
+google.maps.event.addDomListener(window, 'load', init);
+  
   marker.addListener('click', function() {
     infowindow.open(myMap, marker);
   });
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+
+
 
